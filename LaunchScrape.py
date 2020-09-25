f3908f43549a7b2652e4369cabffd01176edc23a
Neil-Ipsen <ipsen.neil@gmail.com>
2020-09-25 14:31:44 +0000

    Created procedural event insertion

--- LaunchScrape.py
+++ LaunchScrape.py
@@ -3,6 +3,28 @@ from bs4 import BeautifulSoup
 from apiclient.discovery import build
 from httplib2 import Http 
 from oauth2client import file, client, tools
+import pytz
+from datetime import datetime, timedelta
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store, flags) \
+        if flags else tools.run(flow, store)
+CAL = build('calendar', 'v3', http=creds.authorize(Http()))
+
+def is_dst(zonename):
+    tz = pytz.timezone(zonename)
+    now = pytz.utc.localize(datetime.utc_now())
+    return now.astimezone(tz).dst() != timedelta(0)
 
 def siteParse():
     # Fetch source URL
@@ -45,7 +67,20 @@ def siteParse():
     missionWindow = split[2]
     missionDescription = split[3]
 
-    print(split)
+    if is_dst("America/New_York"):
+        GMT_OFF = '-04:00'
+    else:
+        GMT_OFF = '-05:00'
+    
+    for i in split:
+        EVENT = {
+            'summary': split[3],
+            'start':   {'dateTime': split[0] % GMT_OFF},
+            'end':     {'dateTime': split[0] % GMT_OFF}
+        }
+    
+        e = CAL.events().insert(calendarId='primary', sendNotifications=True, body=EVENT).execute()
+    
     return
 
 siteParse()
